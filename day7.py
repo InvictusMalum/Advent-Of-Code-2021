742f3487b8ed9c8f8dae67d4fd76b46caaa00568
InvictusMalum <tylerjweed@gmail.com>
2021-12-07 10:33:56 -0500

    day7NotDone

--- day7.py
+++ day7.py
@@ -1,2 +1,18 @@
+import statistics
+
 f = open("day7input.txt", "r")
 input = f.read()
+
+positions = [int(i) for i in input.split(",")]
+
+def totalDeviation(positions, number):
+    total = 0
+    for position in positions:
+        total += abs(number-position)
+    return total
+
+deviations = []
+for i in range(max(positions)):
+    deviations.append(totalDeviation(positions, i))
+
+print(min(deviations))
