f8acb7985d22187b0b3af189f49bbff3ac4ace25
InvictusMalum <tylerjweed@gmail.com>
2021-12-07 12:26:31 -0500

    Day7

--- day7.py
+++ day7.py
@@ -5,14 +5,32 @@ input = f.read()
 
 positions = [int(i) for i in input.split(",")]
 
+# Cost = Steps
 def totalDeviation(positions, number):
     total = 0
     for position in positions:
         total += abs(number-position)
     return total
 
+def costOfStep(step):
+    return step*(step+1)//2
+
+def totalFuelForIncreasingCost(positions, number):
+    total = 0
+    for position in positions:
+        total += costOfStep(abs(number-position))
+    return total
+
+
 deviations = []
 for i in range(max(positions)):
     deviations.append(totalDeviation(positions, i))
+
+print(min(deviations))
+
+
+deviations = []
+for i in range(max(positions)):
+    deviations.append(totalFuelForIncreasingCost(positions, i))
 
 print(min(deviations))
