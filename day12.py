977eb134b15a0d93bca598e8429e0c41924ec14e
InvictusMalum <tylerjweed@gmail.com>
2021-12-13 13:20:36 -0500

    Day12

--- day12.py
+++ day12.py
@@ -1,2 +1,39 @@
 f = open("day12input.txt", "r")
 input = f.read()
+
+pathCodes = input.split("\n")
+
+def pathDictAndCavesList(pathCodes):
+    paths = {}
+    caves = []
+    
+    for createCommand in pathCodes:
+        points = createCommand.split("-")
+        if points[0] not in caves:
+            caves.append(points[0])
+            paths[points[0]] = []
+        if points[1] not in caves:
+            caves.append(points[1])
+            paths[points[1]] = []
+        
+        paths[points[0]].append(points[1])
+        paths[points[1]].append(points[0])
+
+    return paths, caves
+
+def isSmallCave(cave):
+    if cave.lower() == cave:
+        return True
+    return False
+
+def pathsToEnd(pathsDict, start):
+    if start == 'end':
+        return 'end'
+    else:
+        paths = []
+        for element in pathsDict[start]:
+            for path in pathsToEnd(pathsDict, element):
+                paths.append(['start'] + path)
+
+paths, caves = pathDictAndCavesList(pathCodes)
+print(pathsToEnd(paths, 'start'))
