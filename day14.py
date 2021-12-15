7c2e7bb0cb900ba6a5b232b8d4f12e70ad49d256
InvictusMalum <tylerjweed@gmail.com>
2021-12-15 13:43:14 -0500

    Day14P1

--- day14.py
+++ day14.py
@@ -11,25 +11,54 @@ def createPairDict(pairs):
     for pairCode in pairs:
         pairCode = pairCode.split(" -> ")
         dict[pairCode[0]] = pairCode[1]
-    for i in dict:
-        print(i, dict[i])
     return dict
 
 def polymerStep(polymerString, pairDict):
     outString = ''
     for i in range(len(polymerString)-1):
         if polymerString[i:i+2] in pairDict:
-            outString += polymerString[i:i+1] + pairDict[polymerString[i:i+2]] + polymerString[i:i+1]
+            outString += polymerString[i] + pairDict[polymerString[i:i+2]]
         else:
-            outString += polymerString[i:i+2]
+            outString += polymerString[i]
+    outString += polymerString[-1]
     return outString
 
 def applySteps(stepNum, startPolymer, pairDict):
-    for i in stepNum:
+    for i in range(stepNum):
         startPolymer = polymerStep(startPolymer, pairDict)
+        print(i)
     return startPolymer
+
+def countLetters(string):
+    counts = {}
+    for i in string:
+        if i in counts:
+            counts[i] += 1
+        else:
+            counts[i] = 1
+    
+    return counts
+
+def countMost(string):
+    counts = countLetters(string)
+    max = 0
+    for i in counts:
+        if counts[i] > max:
+            max = counts[i]
+    
+    return max
+
+def countLeast(string):
+    counts = countLetters(string)
+    min = len(string)
+    for i in counts:
+        if counts[i] < min:
+            min = counts[i]
+
+    return min
+
 
 pairDict = createPairDict(pairs)
 
-print(startPolymer)
-print(polymerStep(startPolymer, dict))
+startPolymer = applySteps(40, startPolymer, pairDict)
+print(countMost(startPolymer) - countLeast(startPolymer))
