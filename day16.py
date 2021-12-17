2a08f073f1af76fbce63d3af473d2cd7965864ce
InvictusMalum <tylerjweed@gmail.com>
2021-12-17 13:18:54 -0500

    Day16Mid

--- day16.py
+++ day16.py
@@ -1,3 +1,5 @@
+from typing import Pattern
+
 f = open("day16input.txt", "r")
 input = f.read()
 
@@ -38,30 +40,41 @@ def binaryToInt(binaryString):
 class PacketHolder():
     subPackets = []
 
+    def versionSum():
+        out = 0
+        for packet in PacketHolder.subPackets:
+            out += packet.versionSum()
+        return out
+
+    def evaluate():
+        for packet in PacketHolder.subPackets:
+            packet.evaluate()
+        return PacketHolder.subPackets[0].value
+
+
 class Packet():
     def __init__(self, string, parent = PacketHolder):
         self.string = string
         self.parent = parent
 
-        if self.parent == PacketHolder:
-            self.parent.subPackets.append(self)
+        self.parent.subPackets.append(self)
 
         if '1' not in self.string:
-            parent.subPackets.remove(self)
+            self.parent.subPackets.remove(self)
         else:
             self.create()
-        
+
     def create(self):
-        self.version = self.string[:3]
-        self.typeID = self.string[3:6]
+        self.version = binaryToInt(self.string[:3])
+        self.typeID = binaryToInt(self.string[3:6])
         
-        self.values = []
+        self.value = 0
         self.subPackets = []
 
         # Literal Value
-        if binaryToInt(self.typeID) == 4:
-            leftOver = self.getValues(self.string[6:])
-            self.parent.subPackets.append(Packet(leftOver))
+        if self.typeID == 4:
+            leftOver = self.getLiteralLeftover(self.string[6:])
+            Packet(leftOver, parent=self.parent)
         
         # Operator
         else:
@@ -70,27 +83,99 @@ class Packet():
             # next 15 bits encode the total length in bits of the sub-packets in the packet
             if lengthTypeID == '0':
                 totalLengthOfSubPackets = binaryToInt(self.string[7:22])
-                self.subPackets.append(Packet(self.string[22:22+totalLengthOfSubPackets]))
-                self.parent.subPackets.append(Packet(self.string[22+totalLengthOfSubPackets:]))
+                Packet(self.string[22:22+totalLengthOfSubPackets], parent=self)
+                Packet(self.string[22+totalLengthOfSubPackets:], parent=self.parent)
             # Next 11 bits are the number of subpackets contained in the packet
             else:
-                pass
+                numSubPackets = binaryToInt(self.string[7:18])
+                Packet(self.string[18:], parent = self)
+                if len(self.subPackets) > numSubPackets:
+                    for i in range(numSubPackets,len(self.subPackets)):
+                        self.parent.subPackets.append(self.subPackets[numSubPackets])
+                        self.subPackets.pop(numSubPackets)
 
-    def getValues(self, string):
+    def getLiteralValue(self, string):
+        stop = False
+        values = []
+        while not stop:
+            if string[0] == '0':
+                stop = True
+            values.append(string[1:5])
+            string = string[5:]
+        decimalString = ''
+        for digit in values:
+            decimalString += str(binaryToInt(digit))
+        value = int(decimalString)
+        return value
+
+    def getLiteralLeftover(self, string):
         stop = False
         while not stop:
             if string[0] == '0':
                 stop = True
-            self.values.append(string[1:5])
             string = string[5:]
-        leftOver = string
-        return leftOver
-            
+        return string
+    
+    def evaluate(self):
+        if self.typeID == 4:
+            self.value = self.getLiteralValue(self.string)
 
+        # Sum of all packets
+        elif self.typeID == 0:
+            out = 0
+            for packet in self.subPackets:
+                packet.evaluate()
+                out += packet.value
+            self.value = out
+        
+        # Product of packets
+        elif self.typeID == 1:
+            product = 1
+            for packet in self.subPackets:
+                packet.evaluate()
+                product *= packet.value
 
+        # Minimum packet value
+        elif self.typeID == 2:
+            valueList = []
+            for packet in self.subPackets:
+                packet.evaluate()
+                valueList.append(packet.value)
+            self.value = min(valueList)
+
+        # Maximum
+        elif self.typeID == 3:
+            valueList = []
+            for packet in self.subPackets:
+                packet.evaluate()
+                valueList.append(packet.value)
+            self.value = max(valueList)
+
+        # >
+        elif self.typeID == 5:
+            self.subPackets[0].evaluate()
+            self.subPackets[1].evaluate()
+            if self.subPackets[0].value > self.subPackets[1].value:
+                self.value = 1
+        elif self.typeID == 6:
+            self.subPackets[0].evaluate()
+            self.subPackets[1].evaluate()
+            if self.subPackets[0].value < self.subPackets[1].value:
+                self.value = 1
+        elif self.typeID == 7:
+            self.subPackets[0].evaluate()
+            self.subPackets[1].evaluate()
+            if self.subPackets[0].value == self.subPackets[1].value:
+                self.value = 1
+
+    def versionSum(self):
+        out = self.version
+        for packet in self.subPackets:
+            out += packet.versionSum()
+        return out
 
 def parsePacket(packetString):
     pass
 
-print(hexToBinary('D2FE28'))
-print(Packet('00111000000000000110111101000101001010010001001000000000').subPackets)
+Packet(hexToBinary(input))
+print(PacketHolder.evaluate())
